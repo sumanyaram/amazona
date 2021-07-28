90f8a79ce8c2baae977d19ecf3559cf0d9addc19
Venkata Suman Yaram <sumanyaram@gmail.com>
2021-07-28 13:44:39 +0530

    add router and product pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import Product from './components/product';
-import data from './data';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
     return (
+        <BrowserRouter>
         <div className="grid-container">
         <header className="row">
             <div>
@@ -17,20 +19,14 @@ function App() {
 
         </header>
         <main>
-            <div>
-                <div className="row center">
-                    {
-                        data.products.map((product) => (
-                        <Product key={product._id} product={product}></Product> 
-                        ) )
-                    }                                       
-                </div>
-            </div>
+            <Route path="/product/:id" component={ProductScreen}></Route>            
+            <Route path="/" component={HomeScreen} exact={true}></Route>            
         </main>
         <footer className="row center">
             All rights reserved @
         </footer>
     </div>
+    </BrowserRouter>
     );
 }
 
