86d6c53d13f92ff42fab248531a027aa721206e5
Venkata Suman Yaram <sumanyaram@gmail.com>
2021-08-01 15:46:24 +0530

    place order screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { signout } from './actions/userActions';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import ProductScreen from './screens/ProductScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
@@ -63,7 +64,8 @@ function App() {
             <Route path="/signin" component={SigninScreen}></Route>      
             <Route path="/register" component={RegisterScreen}></Route>
             <Route path="/shipping" component={ShippingAddressScreen}></Route>
-            <Route path="/payment" component={PaymentMethodScreen}></Route>     
+            <Route path="/payment" component={PaymentMethodScreen}></Route>  
+            <Route path="/placeorder" component={PlaceOrderScreen}></Route>
             <Route path="/" component={HomeScreen} exact={true}></Route>            
         </main>
         <footer className="row center">
